1f4695590093f320ca5043c7f52774345b38e5e9
Alkihis <alkihis@gmail.com>
2019-10-08 11:09:32 +0200

    test orms dans get_student

--- src/get_student.py
+++ src/get_student.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, Response
 from helpers import getDb
 from server import Etudiant, db_session
+from datetime import date
 
 ### Define a classic route, and receive Flask app
 def get_student_routes(app: Flask):
@@ -10,4 +11,16 @@ def get_student_routes(app: Flask):
     e: Etudiant = Etudiant.query.filter_by(id_etu=identifier).one_or_none()
 
     return jsonify(e)
+
+  @app.route('/student/create/<nom>/<prenom>', methods=['GET', 'PUT'])
+  def putStudent(nom: str, prenom: str) -> Response:
+    etu = Etudiant.create(nom, prenom, "hello@pouet.fr", date.today(), "1902/1903")
+
+    db_session.add(etu)
+    db_session.commit()
+
+    return jsonify(etu)
     
+  @app.route('/student/all')
+  def allStudents():
+    return jsonify(Etudiant.query.all())
