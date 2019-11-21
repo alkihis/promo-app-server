e9bee183a46233e0249e76175f3028af04c6d58d
Louis Béranger <alkihis@gmail.com>
2019-11-21 14:41:41 +0100

    Contact fixes

--- src/Vues/contact.py
+++ src/Vues/contact.py
@@ -7,7 +7,7 @@ from errors import ERRORS
 from server import db_session
 from typing import List, Tuple, Dict
 from sqlalchemy import and_, or_
-
+import re
 
 def define_contact_endpoints(app: flask.Flask):
   @app.route('/contact/create', methods=["POST"])
@@ -25,11 +25,15 @@ def define_contact_endpoints(app: flask.Flask):
     name, mail, id_entreprise = data['name'], data['mail'], data['id_entreprise']
 
     ## Search for similar contact TODO improve search
-    f = Contact.query.filter(and_(Contact.nom.ilike(f"{name}"), Contact.mail.ilike(f"{mail}"),
-                                  Contact.id_entreprise.ilike(f"{entreprise}"))).all()
+    f = Contact.query.filter(
+      and_(
+        Contact.nom.ilike(f"{name}"), 
+        Contact.mail.ilike(f"{mail}"), 
+        Contact.id_entreprise.ilike(f"{id_entreprise}")
+      )
+    ).all()
       
     if len(f):
-      attach_previous_contact(user_id, f[0].id_entreprise)
       return flask.jsonify(f[0]), 200
 
     #Check id_entreprise
@@ -40,7 +44,7 @@ def define_contact_endpoints(app: flask.Flask):
     
 
     email_catch = r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$" 
-    if not re.match(email_catch, email):
+    if not re.match(email_catch, mail):
       return ERRORS.BAD_REQUEST
 
     # Create new contact
@@ -66,52 +70,3 @@ def define_contact_endpoints(app: flask.Flask):
       return ERRORS.MISSING_PARAMETERS
 
     return flask.jsonify(Contact.query.filter_by(id_entreprise=id_e).all())
-
-
-  @app.route('/contact/related')
-  @login_required
-  def find_relative_contact():
-    r = get_request()
-    name: str = None
-    mail: str = None
-    id_entreprise: int = None
-    included = False
-
-    if 'name' in r.args:
-      name = r.args['name']
-    if 'mail' in r.args:
-      mail = r.args['mail']
-    if 'id_entreprise' in r.args:
-        id_entreprise = r.args['id_entreprise']
-    if 'included' in r.args and is_truthy(r.args['included']):
-      included = True
-
-    # get all contacts
-    contacts: List[Contact] = Contact.query.all()
-
-    accepted: List[Tuple[Contact, Dict[str, float]]] = []
-
-    # Find contacts that matches the query
-    for f in contacts:
-      dist_name = 0
-      dist_mail = 0
-      if name:
-        dist_name = Levenshtein.ratio(f.nom, name)
-      if mail:  
-        dist_mail = Levenshtein.ratio(f.mail, mail)
-
-      if dist_name >= 0.6 or dist_mail >= 0.6:
-        accepted.append((f, {'name': dist_name, 'location': dist_mail}))
-        continue
-      
-      # Search for substrings
-      if included:
-        if name and name in f.nom:
-          accepted.append((f, {'name': dist_name, 'location': dist_mail}))
-        elif mail and location in f.mail:
-          accepted.append((f, {'name': dist_name, 'location': dist_mail}))
-
-    # Sort the accepted by max between dist_name and dist_location
-    accepted.sort(key=lambda x: max((x[1]['name'], x[1]['mail'])), reverse=True)
-
-    return flask.jsonify(accepted)
